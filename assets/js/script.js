a797ee2f5c5e776961b92dbbda28c0f2762b9df7
Phil Zieger <phil.zieger.pro@gmail.com>
2022-12-15 18:27:01 +0100

    Add login page and new Admin system

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,117 @@
-if (window.fetch) {
-    fetch('http://localhost:5678/api/works').then(response => 
-    response.json().then(data => ({
-      data: data,
-      status: response.status
-    })
-    ).then(res => {
-      works = res;
-      ListingWorks(0);
-      
-    }));
+let modal = null
 
-} else {
-    // Faire quelque chose avec XMLHttpRequest?
+const openModal = function (e) {
+  
+  e.preventDefault()
+  const target = document.querySelector(e.target.getAttribute('href'))
+  target.style.display = null
+  target.removeAttribute('aria-hidden')
+  target.setAttribute('aria-modal', 'true')
+  modal = target
+  modal.addEventListener('click', closeModal)
+  modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+  modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+  if (modal === null) return
+  e.preventDefault() 
+  modal.style.display = "none"
+  modal.setAttribute('aria-hidden', 'true')
+  modal.removeAttribute('aria-modal')
+  modal.removeEventListener('click', closeModal)
+  modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+  modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+  modal = null
+}
+
+const stopPropagation = function (e) {
+  e.stopPropagation()
 }
 
 
+document.querySelectorAll('.js-modal').forEach(a => {
+  a.addEventListener('click', openModal)
+})
 
 
+function onload() {
+  if (window.fetch) {
+      fetch('http://localhost:5678/api/works').then(response => 
+      response.json().then(data => ({
+        data: data,
+        status: response.status
+      })
+      ).then(res => {
+        works = res;
+        ListingWorks(0);
+        loadAdminPage();
+        
+      }));
+
+  } else {
+      // Faire quelque chose avec XMLHttpRequest?
+  }
+  
+}
+
+function Logout() {
+  document.cookie = 'admin=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+  document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+}
+
+function loadAdminPage() {
+  //Load admin page if connected
+  if (recupererCookie("admin") == "true") {
+    var edition = document.getElementById('edition');
+    edition.style.display = 'contents';
+    var menuIn = document.getElementById('loginButton');
+    var menuOut = document.getElementById('logoutButton');
+    menuIn.style.display = 'none';
+    menuOut.style.display = 'contents';
+    var ButtonEdit = document.getElementById('adminedit');
+    ButtonEdit.style.display = 'contents';
+  }
+}
+
+async function loginuser(email, password) {
+  user = JSON.stringify({
+      email: email,
+      password: password
+  })
+    
+    let response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: user
+    });
+    let result = await response.json();
+    console.log(response.status);
+    if (response.status == 200 ) {
+      //Connected
+      token = result.token;
+      document.cookie = "token=" + token;
+      document.cookie = "admin=" + "true";
+      document.location.href="index.html"; 
+    } else if ( response.status == 401 || response.status == 404) {
+      //Rong e-mail or password
+      const remoteRongElement = document.getElementById('rong');
+      remoteRongElement.textContent = "Votre identifiant est incorrecte !";
+    }
+}
+
+function recupererCookie(nom) {
+  nom = nom + "=";
+  var liste = document.cookie.split(';');
+  for (var i = 0; i < liste.length; i++) {
+      var c = liste[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nom) == 0) return c.substring(nom.length, c.length);
+  }
+  return null;
+}
 function ListingWorks(category) {
 
   if (category == 0 ) {
